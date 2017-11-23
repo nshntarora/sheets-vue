ef38eb581f831f7767e9379993a29f26efd157fd
Nishant Arora <na.nishantarora@gmail.com>
2017-11-23 19:58:40 +0000

    Added cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <input class="form-control" :value="value" @input="handleInput">
+  </input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cell',
+  props: ['value'],
+  methods: {
+    handleInput(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
+};
+</script>
+
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <h1>Sheets</h1>
+    <cell v-model="msg"></cell>
   </div>
 </template>
 
 <script>
+import Cell from './Cell';
+
 export default {
   name: 'displayTable',
   data() {
     return {
       msg: 'Sheets',
     };
+  },
+  components: {
+    Cell,
   },
 };
 </script>
