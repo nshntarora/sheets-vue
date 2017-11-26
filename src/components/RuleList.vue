9e12d71b53b39ab186b427f074827242fef2cf73
Nishant Arora <na.nishantarora@gmail.com>
2017-11-26 21:26:32 +0000

    No rules message

--- src/components/RuleList.vue
+++ src/components/RuleList.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    <div class="card" v-for="(rule, index) in rules" :key="rule.id">
-      <div class="card-body">
-        <i class="fa fa-trash float-right pointer" @click.prevent.stop="deleteRule(rule.id)"></i>
-        #{{ index+1 }} <b>{{ columns[rule.column] }}</b>
+  <div class="card">
+    <div class="card-header">
+        Added Rules
+      </div>
+    <div class="card-body">
+      <div v-if="rules.length > 0">
+      <div class="card" v-for="(rule, index) in rules" :key="rule.id">
+        <div class="card-body">
+          <i class="fa fa-trash float-right pointer" @click.prevent.stop="deleteRule(rule.id)"></i>
+          #{{ index+1 }} <b>{{ columns[rule.column] }}</b>
+        </div>
+      </div>
+      </div>
+      <div class="alert alert-primary" role="alert" v-else>
+        There are no rules added yet.
       </div>
     </div>
   </div>
